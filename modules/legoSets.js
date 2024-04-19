b794f71b9f6dd2fb648d8defbaeb6dff07e4e00a
Nehmat Ladhar <144140071+nladhar@users.noreply.github.com>
2024-04-19 19:41:25 -0400

    Update legoSets.js

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -1,10 +1,10 @@
 /*********************************************************************************
-*  WEB322 – Assignment 5
+*  WEB322 – Assignment 6
 *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
-*  Name: Nehmat Ladhar  Student ID: 137500229 Date: 12-04-24
+*  Name: Nehmat Ladhar  Student ID: 137500229 Date: 19-04-24
 *
 ********************************************************************************/
 
